655bd7b060e761e86e238d16550b75abce9d0375
gitjb-cyber <dodo9726@gmail.com>
2024-10-11 09:32:12 +0900

    Add files via upload

---
+++ day01/src/exam01/Ex04.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        int num1 = 100;
+        num1 = 200;
+        System.out.println(num1);
+
+        final int NUM = 200; // 상수
+        final int MAX_NUMBER;
+
+    }
+}
---
+++ day01/src/exam01/Ex07.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        /*long num1 = 10000000000;*/ // int -> long
+        long num1 = 10000000000L; // 처음부터 long 으로 인식
+        byte num2 = 100; // int 100 -> byte 변환
+        long num3 = 10_000_000_000L;
+
+    }
+}
